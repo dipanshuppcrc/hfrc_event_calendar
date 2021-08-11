eee5b1a974800307780cf6cef83a09774c211bac
Nikhil Kandekar <nikhilbkandekar@gmail.com>
2021-08-11 15:04:51 +0530

    Edited Doctype

--- hfrc_event_calendar/hfrc_event_calendar/doctype/hfrc_events/hfrc_events.js
+++ hfrc_event_calendar/hfrc_event_calendar/doctype/hfrc_events/hfrc_events.js
@@ -2,7 +2,18 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('HFRC Events', {
-	// refresh: function(frm) {
+	category_type: function (frm) {
+		let category = frm.doc.category_type
 
-	// }
+		frm.set_query('sub_category', () => {
+			return {
+				"filters": {
+					"category": category
+				}
+			}
+		})
+	},
+	location: function (frm) {
+		frm.toggle_display('other_location', frm.doc.location === 'Other')
+	}
 });
