b5cedc604a3c46cf4759bc6837a7b0ee0e3a94f4
Nikhil-Kandekar <nikhilbkandekar@gmail.com>
2021-08-26 11:24:20 +0530

    hide row

--- hfrc_event_calendar/hfrc_event_calendar/doctype/hfrc_events/hfrc_events.js
+++ hfrc_event_calendar/hfrc_event_calendar/doctype/hfrc_events/hfrc_events.js
@@ -15,5 +15,13 @@ frappe.ui.form.on('HFRC Events', {
 	},
 	location: function (frm) {
 		frm.toggle_display('other_location', frm.doc.location === 'Other')
+	},
+	onload_post_render: function(frm){
+		// var count=0;
+		// console.log("Image Added");
+		// console.log(++count);
+		// console.log($('[data-fieldname="image"] > .form-clickable-section > .flex > .grid-buttons > .grid-add-row').text());
+		// $('[data-fieldname="image"] > .form-clickable-section > .flex > .grid-buttons > .grid-add-row').hide();
+
 	}
 });
